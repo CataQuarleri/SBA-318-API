cf033c4fc0179b2d2b192e81545d99fba4367e66
CataQu <cataquarleri@gmail.com>
2024-04-26 21:55:46 -0600

    cloned from trial CRM-API. Starting address routes

--- src/routes/addressRoutes.js
+++ src/routes/addressRoutes.js
@@ -6,5 +6,24 @@ router
     .get((req, res, next) => {
         console.log("IM IN GET of address")
     })
+    .post((req, res) => {
+		let street = req.body.street;
+		let city = req.body.city;
+		let state = req.body.state;
+		let zip = req.body.zip;
+		let country = req.body.country;
+		let typeOfAddress = req.body.type;
+		if(service && startingDate && endDate && street && city && state && zip && country && typeOfAddress) {
+			let id = (addressesArray.length -1) + 1;
+			let address = {id: id, typeOfAddress, street, city, state, zip, country}
+			let addInfo = {service, nextDates: {startingDate, endDate}, address}
+			console.log("ADD INFO", addInfo)
+			store.set('addInfo', {addInfo: addInfo})
+			store.set('address', {address: address})	
+			res.json({addInfo: addInfo, address: address})
+		}else {
+			res.json({error: "Not enough information"})
+		}
+	})
 
 module.exports = router;
--- src/routes/usersRoutes.js
+++ src/routes/usersRoutes.js
@@ -9,29 +9,24 @@ const addressesArray = require('../../data/address');
 let phonePattern = '^[0-9]+$'; //I need the phone as a string so I use pattern instead of type number in input
 let emailPattern = '^[^@]+@[^@]+.[^@]+$';
 
-//UI routes
+//Api routes
 router
-	.route('/quote')
+	.route('/api')
 	.get((req, res, next) => {
-		let hasContactInfo = store.get('hasContactInfo');
-		if (!hasContactInfo?.hasContactInfo) {
-			res.render('pages/createUser.ejs', {
-				// async:true,
-				hasContactInfo: hasContactInfo,
-				pageName: 'Get a Quote',
-				phonePattern: phonePattern,
-				emailPattern: emailPattern
-			});
-		} else {
-			res.redirect('/users/addInfo');
-		}
+		res.json(usersArray);
 	})
-	router.post('/api/contactInfo', (req, res) => {
-		let body = req.body;
+	.post('/contactInfo', (req, res) => {
 		let firstName = req.body.firstName;
 		let lastName = req.body.lastName;
 		let phone = req.body.phone;
+		if (phone.match(phonePattern)) {
+			next(error(400, "Bad request, phone has to be only numbers"))
+		}
 		let email = req.body.email;
+		if (email.match(emailPattern)){
+			next(error(400, "Bad request, email has to be a valid format"))
+
+		}
 		let contactPreferences = req.body.contactPreferences;
 		let _id = (usersArray.length == 0 ? usersArray.length : usersArray.length - 1) + 1;
 		if (firstName && lastName && phone && email && contactPreferences) {
@@ -44,58 +39,12 @@ router
 				contactPreferences: contactPreferences
 			};
 			store.set('contactInfo', data);
-			store.set('hasContactInfo', {hasContactInfo: true});
 			store.set('minUserInfo', {id: _id, name: `${firstName} ${lastName}`, phone: phone})
 		}
-		res.json({contactInfo: "saved", _id: _id, status: 201})
-		// res.redirect('/users/addinfo');
-	});
-
-
-
-router
-	.route('/addinfo')
-	.get((req, res, next) => {
-		let contactInfo = store.get("hasContactInfo")
-		console.log("HAS", contactInfo)
-		res.render('pages/createUser.ejs', {
-			hasContactInfo: contactInfo.hasContactInfo,
-			pageName: 'Get a Quote',
-			phonePattern: phonePattern,
-			emailPattern: emailPattern
-		});
-});
-
-
-//Api routes
-router
-	.route('/api')
-	.get((req, res, next) => {
-		res.json(usersArray);
+		res.json({contactInfo: "saved", _id: _id, status: 201, user: data})
 	})
-	.post((req, res) => {
-		let service = req.body.service;
-		let startingDate = req.body.startingDate;
-		let endDate = req.body.endDate || "No end date";
-		let street = req.body.street;
-		let city = req.body.city;
-		let state = req.body.state;
-		let zip = req.body.zip;
-		let country = req.body.country;
-		let typeOfAddress = req.body.type;
-		if(service && startingDate && endDate && street && city && state && zip && country && typeOfAddress) {
-			let id = (addressesArray.length -1) + 1;
-			let address = {id: id, typeOfAddress, street, city, state, zip, country}
-			let addInfo = {service, nextDates: {startingDate, endDate}, address}
-			console.log("ADD INFO", addInfo)
-			store.set('addInfo', {addInfo: addInfo})
-			store.set('address', {address: address})		
-			res.redirect('/pets');
-			// res.json({addInfo: addInfo, address: address})
-		}else {
-			res.json({error: "Not enough information"})
-		}
-	});
+	
+	
 
 module.exports = router;
 
